ab3dd54941da654728249e341e6989768799cc1f
kssmith9 <kssmith9@ncsu.edu>
2019-11-16 16:23:01 +0700

    Initial Commit

---
+++ Lesson_Wizard_Scrambler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson_Wizard_Scrambler
+//
+//  Created by Kevin Smith on 11/16/19.
+//  Copyright © 2019 Kevin Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
